e0b3a34b8de3a1c599f0dc9495426d8e6629a8a0
Your Name <you@example.com>
2023-07-20 17:27:45 +0200

    edite

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const { readData, writeData } = require('./data');
 app.use(cors());
 require('dotenv').config();
 const port = process.env.PORT || 8000;
-const {readdirSync} = require("fs");
-readdirSync("./routes").map((file)=>app.use("/",require("./routes/"+file)))
+// const {readdirSync} = require("fs");
+// readdirSync("./routes").map((file)=>app.use("/",require("./routes/"+file)))
 
-app.get("/",(req,res)=>{
-    res.send("products");
-});
+// app.get("/",(req,res)=>{
+//     res.send("products");
+// });
+
+
+app.get('/products', (req, res) => {
+    const data = readData();
+    res.json(data);
+  });
+  
+  app.post('/products', (req, res) => {
+    const data = readData();
+    const product = req.body;
+    data.push(product);
+    writeData(data);
+    res.json(product);
+  });
 
 
 app.listen(port,()=>{
